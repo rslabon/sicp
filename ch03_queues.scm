8b314297bd71f0cb4caa4f2e92cb985edf505a1a
Rafal <rslabon@gmail.com>
2015-10-15 22:26:47 +0200

    ex 3.23 - deque

--- ch03_queues.scm
+++ ch03_queues.scm
@@ -86,6 +86,75 @@
 ;((q1 'delete!))
 ;((q1 'delete!))
 
+;ex 3.23
 
+(define (make-deque) (cons `() `()))
+(define (empty-deque? deque) (and (null? (car deque)) (null? (cdr deque))))
+(define (front-deque deque) (car deque))
+(define (rear-deque deque) (cdr deque))
+(define (set-front-deque! deque item) (set-car! deque item))
+(define (set-rear-deque! deque item) (set-cdr! deque item))
+
+(define (make-node value prev-node next-node) (list value prev-node next-node))
+(define (node-value node) (car node))
+(define (node-prev node) (cadr node))
+(define (node-next node) (caddr node))
+(define (set-node-prev! node new-prev-node) (set-car! (cdr node) new-prev-node))
+(define (set-node-next! node new-next-node) (set-car! (cdr (cdr node)) new-next-node))
+
+(define (front-insert-deque! deque item)
+    (cond ((empty-deque? deque)
+           (let ((first-node (make-node item `() `())))
+             (set-front-deque! deque first-node)
+             (set-rear-deque! deque first-node)))
+	  (else
+           (let ((new-front-node (make-node item `() (front-deque deque))))
+             (set-node-prev! (front-deque deque) new-front-node)
+             (set-front-deque! deque new-front-node))))
+  deque)
+
+(define (rear-insert-deque! deque item)
+    (cond ((empty-deque? deque)
+           (let ((first-node (make-node item `() `())))
+             (set-front-deque! deque first-node)
+             (set-rear-deque! deque first-node)))
+	  (else
+	   (let ((new-last-node (make-node item (rear-deque deque) `())))
+	     (set-node-next! (rear-deque deque) new-last-node)
+	     (set-rear-deque! deque new-last-node))))
+  deque)
+
+(define (front-delete-deque! deque)
+  (cond ((empty-deque? deque)
+         (display "empty deque!"))
+        (else
+           (set-front-deque! deque (node-next (front-deque deque)))
+           (if (not (null? (front-deque deque)))
+               (set-node-prev! (front-deque deque) `())
+               (set-rear-deque! deque `()))))
+  deque)
+
+(define (rear-delete-deque! deque)
+  (cond ((empty-deque? deque)
+         (display "empty deque!"))
+        (else
+           (set-rear-deque! deque (node-prev (rear-deque deque)))
+           (if (not (null? (rear-deque deque)))
+               (set-node-next! (rear-deque deque) `())
+               (set-front-deque! deque `()))))
+  deque)
+            
+(define q1 (make-deque))
+;(empty-deque? q1)
+(front-insert-deque! q1 'a)
+(front-insert-deque! q1 'b)
+(rear-insert-deque! q1 'c)
+(rear-insert-deque! q1 'd)
+;(front-delete-deque! q1)
+;(front-delete-deque! q1)
+(rear-delete-deque! q1)
+(rear-delete-deque! q1)
+(rear-delete-deque! q1)
+(rear-delete-deque! q1)
 
 
