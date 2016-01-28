f26fe5041efcdc8cc983445feef5e250243b55cc
Rafal Slabon <rafal.slabon@nokia.com>
2016-01-28 17:58:15 +0100

    wip

--- ch03_streams.scm
+++ ch03_streams.scm
@@ -505,3 +505,20 @@
 (define zero-crossings (make-zero-crossings (smooth sense-data)))
 
 ;(n-display-stream zero-crossings 12)
+
+; STREAMS AND DELAYED EVALUATION
+
+(define (integral delayed-integrand initial-value dt)
+  (define int 
+    (cons-stream initial-value
+                 (delay (let ((integrand (force delayed-integrand)))
+                          (add-streams (scale-stream integrand dt)
+                                       int)))))
+  int)
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+(stream-ref (solve (lambda (y) y) 1 0.001) 1000)
